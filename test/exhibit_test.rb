b28d866afd0af8764dd9099fe02e76ea74c21101
Margo Flewelling <flewelling.margo@gmail.com>
2020-02-19 09:14:34 -0700

    Delete interaction pattern comments

--- test/exhibit_test.rb
+++ test/exhibit_test.rb
@@ -17,32 +17,6 @@ class ExhibitTest < Minitest:: Test
   def test_it_has_attrs
     assert_equal "Gems and Minerals", @exhibit.name
     assert_equal 0, @exhibit.cost
-  end 
+  end
 
 end
-
-  #
-  # pry(main)> exhibit.name
-  # # => "Gems and Minerals"
-  #
-  # pry(main)> exhibit.cost
-  # # => 0
-  #
-  # pry(main)> patron_1 = Patron.new("Bob", 20)
-  # # => #<Patron:0x00007fcb13b5c7d8...>
-  #
-  # pry(main)> patron_1.name
-  # # => "Bob"
-  #
-  # pry(main)> patron_1.spending_money
-  # # => 20
-  #
-  # pry(main)> patron_1.interests
-  # # => []
-  #
-  # pry(main)> patron_1.add_interest("Dead Sea Scrolls")
-  #
-  # pry(main)> patron_1.add_interest("Gems and Minerals")
-  #
-  # pry(main)> patron_1.interests
-  # # => ["Dead Sea Scrolls", "Gems and Minerals"]
