b8fb68afaa2df832340ec3bb9c389bba7ef54774
Margo Flewelling <flewelling.margo@gmail.com>
2020-02-19 09:44:11 -0700

    Add test for adding patrons

--- test/museum_test.rb
+++ test/museum_test.rb
@@ -17,6 +17,8 @@ class MuseumTest < Minitest:: Test
     @patron_1.add_interest("Gems and Minerals")
     @patron_2 = Patron.new("Sally", 20)
     @patron_2.add_interest("IMAX")
+    @patron_3 = Patron.new("Johnny", 5)
+    @patron_3.add_interest("Dead Sea Scrolls")
   end
 
   def test_it_exists
@@ -43,4 +45,44 @@ class MuseumTest < Minitest:: Test
     assert_equal [@imax], @dmns.recommend_exhibits(@patron_2)
   end
 
+  def test_admitting_patrons
+    assert_equal [], @dmns.patrons
+
+    @dmns.admit(@patron_1)
+    @dmns.admit(@patron_2)
+    @dmns.admit(@patron_3)
+    assert_equal [@patron_1, @patron_2, @patron_3], @dmns.patrons
+  end
+
 end
+
+
+
+# pry(main)> dmns.patrons_by_exhibit_interest
+# # =>
+# # {
+# #   #<Exhibit:0x00007fb202238618...> => [#<Patron:0x00007fb2011455b8...>],
+# #   #<Exhibit:0x00007fb202248748...> => [#<Patron:0x00007fb2011455b8...>, #<Patron:0x00007fb20227f8b0...>, #<Patron:0x6666fb20114megan...>],
+# #   #<Exhibit:0x00007fb20225f8d0...> => []
+# # }
+#
+# pry(main)> dmns.ticket_lottery_contestants(dead_sea_scrolls)
+# # => [#<Patron:0x00007fb2011455b8...>, #<Patron:0x6666fb20114megan...>]
+#
+# pry(main)> dmns.draw_lottery_winner(dead_sea_scrolls)
+# # => "Johnny" or "Bob" can be returned here. Fun!
+#
+# pry(main)> dmns.draw_lottery_winner(gems_and_minerals)
+# # => nil
+#
+# #If no contestants are elgible for the lottery, nil is returned.
+#
+# pry(main)> dmns.announce_lottery_winner(dead_sea_scrolls)
+# # => "Bob has won the Dead Sea Scrolls exhibit lottery"
+#
+# # The above string should match exactly, you will need to stub the return of `draw_lottery_winner` as the above method should depend on the return value of `draw_lottery_winner`.
+#
+# pry(main)> dmns.announce_lottery_winner(gems_and_minerals)
+# # => "No winners for this lottery"
+#
+# # If there are no contestants, there are no winners.
