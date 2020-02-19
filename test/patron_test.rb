45ddc95939767d4f90b227f948324340775c5402
Margo Flewelling <flewelling.margo@gmail.com>
2020-02-19 09:15:44 -0700

    Delete interation pattern comments

--- test/patron_test.rb
+++ test/patron_test.rb
@@ -1,8 +1,6 @@
 require 'minitest/autorun'
 require 'minitest/pride'
-require './lib/exhibit'
 require './lib/patron'
-
 
 class ExhibitTest < Minitest:: Test
 
@@ -27,24 +25,3 @@ class ExhibitTest < Minitest:: Test
   end
 
 end
-
-
-
-  # pry(main)> patron_1 = Patron.new("Bob", 20)
-  # # => #<Patron:0x00007fcb13b5c7d8...>
-  #
-  # pry(main)> patron_1.name
-  # # => "Bob"
-  #
-  # pry(main)> patron_1.spending_money
-  # # => 20
-  #
-  # pry(main)> patron_1.interests
-  # # => []
-  #
-  # pry(main)> patron_1.add_interest("Dead Sea Scrolls")
-  #
-  # pry(main)> patron_1.add_interest("Gems and Minerals")
-  #
-  # pry(main)> patron_1.interests
-  # # => ["Dead Sea Scrolls", "Gems and Minerals"]
