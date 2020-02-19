3ef0dacb1e60a8acbeffbf9494afd5b3b92cba17
Margo Flewelling <flewelling.margo@gmail.com>
2020-02-19 09:23:26 -0700

    Add museum class initialize and add exhibits

---
+++ lib/museum.rb
@@ -0,0 +1,13 @@
+class Museum
+  attr_reader :name, :exhibits
+
+  def initialize(name)
+    @name = name
+    @exhibits = []
+  end
+
+  def add_exhibit(exhibit_name)
+    @exhibits << exhibit_name
+  end
+  
+end
